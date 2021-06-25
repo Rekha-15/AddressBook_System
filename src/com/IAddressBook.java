c714498221c58b1f81bcd0d634a4f34f3dc85974
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-25 17:13:44 -0400

    modified : searching person using person name or state name : .

--- src/com/IAddressBook.java
+++ src/com/IAddressBook.java
@@ -8,24 +8,34 @@
 package com;
 
 /**
- * interface class 
- * calling all functions
+ * interface class calling all functions
  *
  */
 
 public interface IAddressBook {
-	
+
 	void add();
 
 	void display();
 
 	void edit();
-	
+
 	void delete(String firstName);
-	
+
 	void addMultiplePerson();
-	
+
 	void duplicateCheck(String firstName);
-	
+
+	/**
+	 * Uc7: Ability to ensure there is no Duplicate Entry of the same Person in a
+	 * particular Address Book.
+	 */
+	void searchPersonByName(String firstname);
+
+	/**
+	 * Uc8: Ability to search person in a city or state across the multiple
+	 * AddressBook
+	 */
+	void searchPersonByState(String firstname);
 
 }
--- src/com/Runner.java
+++ src/com/Runner.java
@@ -10,17 +10,12 @@ package com;
 import java.util.Scanner;
 
 /**
- * Runner class is a public type
- * Runner class contains main method
- * if condition is true
- * By using switchCase asking user to what task user needs to perform 
- * option will be given to user to select an number
- * 1= adding contact details
- * 2=displaying contact details
- * 3=editing contact details
- * 4=deleting details
- * 5=adding multiple persons
- * 6=ensuring there is no Duplicate Entry of the same Person in a Address Book
+ * Runner class is a public type Runner class contains main method if condition
+ * is true By using switchCase asking user to what task user needs to perform
+ * option will be given to user to select an number 1= adding contact details
+ * 2=displaying contact details 3=editing contact details 4=deleting details
+ * 5=adding multiple persons 6=ensuring there is no Duplicate Entry of the same
+ * Person in a Address Book 7=Ability to search Person
  */
 
 public class Runner {
@@ -33,7 +28,9 @@ public class Runner {
 
 		while (condition == true) {
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("1.add" + "\n" + "2.Display" + "\n" + "3.edit" + "\n" +"4.delete" + "\n" +"5.addMultiplePerson"+ "\n" + "6.duplicateCheck");
+			System.out.println("1.add" + "\n" + "2.Display" + "\n" + "3.edit" + "\n" + "4.delete" + "\n"
+					+ "5.addMultiplePerson" + "\n" + "6.duplicateCheck" + "\n" + "7.searchPersonByName" + "\n"
+					+ "8.searchPersonByState");
 			Scanner option = new Scanner(System.in);
 
 			switch (option.nextInt()) {
@@ -55,10 +52,21 @@ public class Runner {
 				adressBookImplementation.addMultiplePerson();
 				break;
 			case 6:
-				System.out.println("Enter the Name of the person you wants to check, weather that person is present or not");
+				System.out.println(
+						"Enter the Name of the person you wants to check, weather that person is present or not");
 				String name1 = scanner.nextLine();
 				adressBookImplementation.duplicateCheck(name1);
-                break;
+				break;
+			case 7:
+				System.out.println("Enter a Name");
+				String firstName1 = scanner.nextLine();
+				adressBookImplementation.searchPersonByName(firstName1);
+				break;
+			case 8:
+				System.out.println("Enter a name");
+				String state = scanner.nextLine();
+				adressBookImplementation.searchPersonByState(state);
+				break;
 			default:
 				System.out.println("Invalid Input");
 			}
